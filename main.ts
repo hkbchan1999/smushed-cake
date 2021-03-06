bc7e1af8f708d372570d28a9bab3e3c2861e193c
hkbchan1999 <79371545+hkbchan1999@users.noreply.github.com>
2021-03-06 11:27:23 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,21 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, ot
     sprite.say("No! Splat", 300)
     sprite.startEffect(effects.hearts, 300)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+   setPosition()
+})
 let projectile2: Sprite = null
 let projectile: Sprite = null
+let icing: Sprite = null
 let cake = sprites.create(assets.image`grape peace of cake`, SpriteKind.Player)
+icing = sprites.create(assets.image`pink icing`, SpriteKind.Food)
 cake.setPosition(80, 60)
+setPosition()
 scene.setBackgroundColor(3)
 controller.moveSprite(cake)
 cake.setStayInScreen(true)
-let icing = sprites.create(assets.image` pink icing`, SpriteKind.Food)
-icing.setPosition(0, 0)
+
 // spawning cats yummy
 game.onUpdateInterval(2000, function () {
     projectile = sprites.createProjectileFromSide(assets.image`pop corn cat`, randint(-50, -10), randint(10, 50))
@@ -19,3 +25,8 @@ game.onUpdateInterval(2000, function () {
 game.onUpdateInterval(1000, function () {
     projectile2 = sprites.createProjectileFromSide(assets.image`sky dog`, randint(50, 10), randint(10, 50))
 })
+function setPosition() {
+icing.setPosition(randint(0, 160), randint(0, 120))
+    icing.say("I'M AM HERE", 1000)
+}
+    
